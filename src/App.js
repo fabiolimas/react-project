8394e217715cd64d63f61890839b0aea4612f0ae
Fábio Lima <fabiolima01@live.com>
2025-05-19 15:18:16 -0300

    adição de container heder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar/>
      
-
+      <Footer />
     </Router>
   )
 }
--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,21 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link, Route, Routes} from "react-router-dom"
 import styles from './Navbar.module.css'
-import { DiVim } from "react-icons/di"
+import Home from '../pages/Home'
+import Contato from '../pages/Contato'
+import NovoProjeto from "../pages/NovoProjeto"
+import Sobre from "../pages/Sobre"
+import Container from './Container'
 
 function Navbar(){
     return(
         <header>
-            <div className={styles.logo}>Coasts</div>
+            <div className={styles.logo}><Link to='/'>Coasts</Link></div>
     
         <ul className={styles.navbar}>
                 <li className={styles.itens}>
                     <Link to ='/'>Home</Link>
                 </li>
+                <li className={styles.itens}>
+                    <Link to ='/sobre'>Sobre</Link>
+                </li>
+                <li className={styles.itens}>
+                    <Link to ='/novo_projeto'>Novo Projeto</Link>
+                </li>
                  <li className={styles.itens}>
-                    <Link to ='/orcamento'>Orçamento</Link>
+                    <Link to ='/contato'>Contato</Link>
                 </li>
         </ul>
-
+         <Container customClass="min-height">
+            <Routes>
+            
+                <Route exact path='/' element={<Home />}/>
+                <Route exact path='/contato' element={<Contato />} />
+                <Route exact Path='/sobre' element={<Sobre />}/>
+                <Route exact path='/novo_projeto' element={<NovoProjeto />}/>
+            
+            </Routes>
+        </Container>
         </header>
     
 
