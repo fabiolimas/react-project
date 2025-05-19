1885dbb3d05c6de72210173ddd8e4fd58e42668e
Fábio Lima <fabiolima01@live.com>
2025-05-19 16:17:26 -0300

    header e footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './layouts/Navbar';
 import Footer from './layouts/Footer';
+import Container from './layouts/Container'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import NovoProjeto from './pages/NovoProjeto'
+import Projects from './pages/Projects'
+
 
 function App() {
   return (
     <Router>
       <Navbar/>
-     
+     <Container customClass="min-height">
+            <Routes>
+            
+                <Route exact path='/' element={<Home />}/>
+                <Route exact path='/contato' element={<Contato />} />
+                <Route exact path='/empresa' element={<Empresa />} />
+                <Route exact path='/novo_projeto' element={<NovoProjeto />}/>
+                <Route exact path='/projetos' element={<Projects />}/>
+            
+            </Routes>
+        </Container>
       <Footer />
     </Router>
   )
--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,42 +1,33 @@
 import { Link, Route, Routes} from "react-router-dom"
 import styles from './Navbar.module.css'
-import Home from '../pages/Home'
-import Contato from '../pages/Contato'
-import NovoProjeto from "../pages/NovoProjeto"
-import Sobre from "../pages/Sobre"
 import Container from './Container'
+import logo from '../img/costs_logo.png'
 
 function Navbar(){
     return(
-        <header>
-            <div className={styles.logo}><Link to='/'>Coasts</Link></div>
+        <nav className={styles.navbar}>
+       <Container >
+            <Link to='/'><img src={logo} alt="coasts"/></Link>
     
-        <ul className={styles.navbar}>
+        <ul className={styles.list}>
                 <li className={styles.itens}>
                     <Link to ='/'>Home</Link>
                 </li>
                 <li className={styles.itens}>
-                    <Link to ='/sobre'>Sobre</Link>
+                    <Link to ='/projetos'>Projetos</Link>
                 </li>
+
                 <li className={styles.itens}>
-                    <Link to ='/novo_projeto'>Novo Projeto</Link>
+                    <Link to ='/empresa'>Empresa</Link>
                 </li>
-                 <li className={styles.itens}>
+               
+                <li className={styles.itens}>
                     <Link to ='/contato'>Contato</Link>
                 </li>
         </ul>
-         <Container customClass="min-height">
-            <Routes>
-            
-                <Route exact path='/' element={<Home />}/>
-                <Route exact path='/contato' element={<Contato />} />
-                <Route exact Path='/sobre' element={<Sobre />}/>
-                <Route exact path='/novo_projeto' element={<NovoProjeto />}/>
-            
-            </Routes>
+         
         </Container>
-        </header>
-    
+    </nav>
 
     )
 }
